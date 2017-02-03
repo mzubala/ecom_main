f0530eaf9304f6a65a6b553dad423c76a5bc838c
Maciej Zubala <maciej.zubala@gmail.com>
2017-02-03 16:16:28 +0100

    Add to cart ui

--- ecom_test/lib/ecom_test/ui/order_feature.rb
+++ ecom_test/lib/ecom_test/ui/order_feature.rb
@@ -3,14 +3,23 @@ module EcomTest
     class OrderFeature
 
       def initialize
-        @browser =  Watir::Browser.new(:firefox)
+        @browser = Watir::Browser.new(:firefox)
       end
 
       def add_to_cart(count, product_name)
         products = api_client.search(product_name)
         slug = products["products"].first["master"]["slug"]
         @browser.goto(url("/products/#{slug}"))
-        puts @browser.html
+        @browser.text_field(:id, "quantity").set(count)
+        @browser.button(:text, "Add To Cart").click()
+      end
+
+      def get_cart
+        @browser.goto(url("/cart"))
+        line_items = @browser.elements(css: ".line-item").map do |row|
+          {name: row.h4().text(), quantity: row.input().value()}
+        end
+        EcomTest::Model::Order.new(line_items, nil)
       end
 
       private
